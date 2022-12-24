85946984f32d4c8f446eec8307f7703befaafbeb
Yevhen Liaskivskyi <benatanlev@icloud.com>
2022-12-24 19:01:50 +0200

    task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,9 @@
 const listCategories = document.querySelector("#categories").children.length;
 console.log(`Number of categories: ${listCategories}`);
 
-const items = document.querySelectorAll('.item');
+const items = document.querySelectorAll(".item");
 Array.prototype.forEach.call(items, (element) => {
-    const title = element.querySelector('h2').innerHTML;
-    const itemsLength = element.querySelectorAll('li').length;
-    console.log(`Категория: ${title} Elements: ${itemsLength}`);
+    const title = element.querySelector("h2").innerHTML;
+    const itemsLength = element.querySelectorAll("li").length;
+    console.log(`Category: ${title} Elements: ${itemsLength}`);
 });
