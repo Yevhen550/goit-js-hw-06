c9ca6c27e4b81c8680ce2577364e76d8fa4bea64
Yevhen Liaskivskyi <benatanlev@icloud.com>
2022-12-25 16:30:03 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const changeButton = document.querySelector(".change-color");
+const colorNumber = document.querySelector(".color");
+changeButton.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  body.setAttribute("style", `background-color: ${color}`);
+  colorNumber.textContent = color;
+});
