0fbc60815a7cb2027901b1bc19233de7732d9ce0
Yevhen Liaskivskyi <benatanlev@icloud.com>
2022-12-25 17:27:47 +0200

    some change

--- js/task-04.js
+++ js/task-04.js
@@ -1,17 +1,17 @@
-let valueEl = Number(document.querySelector('#value').textContent);
+let valueEl = Number(document.querySelector("#value").textContent);
 
-let counterValue = document.querySelector('#value');
+let counterValue = document.querySelector("#value");
 
-const dekrBtn = document.querySelector('#counter').firstElementChild;
-const inkrBtn = document.querySelector('#counter').lastElementChild;
+const dekrBtn = document.querySelector("#counter").firstElementChild;
+const inkrBtn = document.querySelector("#counter").lastElementChild;
 
-inkrBtn.addEventListener('click', event => {
+inkrBtn.addEventListener("click", (event) => {
   valueEl += 1;
 
   counterValue.innerText = valueEl;
 });
 
-dekrBtn.addEventListener('click', event => {
+dekrBtn.addEventListener("click", (event) => {
   valueEl -= 1;
 
   counterValue.innerText = valueEl;
--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,11 @@
-let inputEl = document.querySelector('#name-input');
-inputEl.addEventListener('input', onInputChange);
+let inputEl = document.querySelector("#name-input");
+inputEl.addEventListener("input", onInputChange);
 
-let outputEl = document.querySelector('#name-output');
+let outputEl = document.querySelector("#name-output");
 
 function onInputChange(event) {
   inputEl = event.currentTarget.value;
   console.log(inputEl);
 
-  inputEl !== ' ' ? (outputEl.innerText = inputEl.trim()) : "Anonymous";
+  inputEl !== " " ? (outputEl.innerText = inputEl.trim()) : "Anonymous";
 }
